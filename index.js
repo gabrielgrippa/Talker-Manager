04d6bd52064606211b5fc9c54468353f9168cb6c
Gabriel Grippa Alves <gabrielgrippa1@gmail.com>
2022-08-15 17:10:11 -0300

    requisito 5 ok

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { talker } = require('./talker');
 const { talkerId } = require('./talkerId');
 const { login } = require('./login');
+const { talkerPost } = require('./talkerPost');
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,6 +20,7 @@ app.get('/', (_request, response) => {
 app.get('/talker', talker);
 app.get('/talker/:id', talkerId);
 app.post('/login', login);
+app.post('/talker', talkerPost);
 
 app.listen(PORT, () => {
   console.log('Online');
